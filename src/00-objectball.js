d96b03bfec4d15f8e5adca20b177037af73cec10
Amber Frantz <amber.k.frantz@mgail.com>
2022-04-10 22:58:14 -0400

    Completed Assingment

---
+++ src/00-objectball.js
@@ -0,0 +1,60 @@
+function gameObject() {
+    return {
+        "home": {
+            "teamName": "Brooklyn Nets",
+            "colors": ["Black", "White"],
+            "players" : {
+                "Alan Anderson": {
+                    "number": 0,
+                    "shoe": 16,
+                    "points": 22,
+                    "rebounds": 12,
+                    "assists": 12,
+                    "steals": 3,
+                    "blocks": 1,
+                    "slamDunks": 1
+                },
+                "Reggie Evans": {                    
+                    "number": 0,
+                    "shoe": 16,
+                    "points": 22,
+                    "rebounds": 12,
+                    "assists": 12,
+                    "steals": 3,
+                    "blocks": 1,
+                    "slamDunks": 1},
+                "Brook Lopez": {
+                    "number": 0,
+                    "shoe": 16,
+                    "points": 22,
+                    "rebounds": 12,
+                    "assists": 12,
+                    "steals": 3,
+                    "blocks": 1,
+                    "slamDunks": 1},},
+                "Mason Pluntee": { 
+                    "number": 1,
+                    "shoe": 16,
+                    "points": 22,
+                    "rebounds": 12,
+                    "assists": 12,
+                    "steals": 3,
+                    "blocks": 1,
+                    "slamDunks": 1},},
+                "Jason Terry": {
+                    "number": 4,
+                    "shoe": 10,
+                    "points": 10,
+                    "rebounds": 1,
+                    "assists": 1,
+                    "steals": 2,
+                    "blocks": 7,
+                    "slamDunks": 2}
+                }
+            },
+    },
+    "away": {
+        "teamName": "",
+        "colors": [""],
+        "players": {}    }
+}
